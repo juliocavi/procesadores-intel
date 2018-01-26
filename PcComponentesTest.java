588fa08e8eb6807065cfe7a0bbe6d4dd6e38c44d
Julio Cachon Villadangos <juliocachonvi@gmail.com>
2018-01-26 11:07:45 +0100

    Añadidos comentarios al test

--- PcComponentesTest.java
+++ PcComponentesTest.java
@@ -43,16 +43,27 @@ public class PcComponentesTest
     @Test
     public void tests()
     {
+        System.out.println("Creamos un nuevo objeto PcComponentes.");
         PcComponentes pcCompon1 = new PcComponentes();
+        System.out.println("###############################################");
+        System.out.println("Añadimos unos cuantos procesadores.");
         pcCompon1.addProcesador("i7 7700", 5000, 2016);
         pcCompon1.addProcesador("i5 6400", 3300, 2015);
         pcCompon1.addProcesador("i3 4100", 2700, 2014);
         pcCompon1.addProcesador("i7 8600", 4500, 2017);
         pcCompon1.addProcesador("i5 6500", 3700, 2016);
+        System.out.println("###############################################");
+        System.out.println("Mostramos la lista de estos.");
         pcCompon1.mostrarCpus();
+        System.out.println("###############################################");
+        System.out.println("Modificamos la frecuencia de los procesadores según su id.");
         pcCompon1.modificarFrecuencia(2, 4000);
         pcCompon1.modificarFrecuencia(4, 4000);
+        System.out.println("###############################################");
+        System.out.println("Eliminamos los procesadores filtrados por año.");
         pcCompon1.eliminacionMedianteIterador(2016);
+        System.out.println("###############################################");
+        System.out.println("Mostramos los procesadores.");
         pcCompon1.mostrarCpus();
     }
 }
